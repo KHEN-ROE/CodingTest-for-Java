d220297b5a7723ebf3598ecf3b34933a00dc03d3
KEN <shdnrnjs@gmail.com>
2023-04-13 08:03:06 +0900

    정렬

---
+++ src/백준/정렬/대표값2.java
@@ -0,0 +1,18 @@
+package 백준.정렬;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 대표값2 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int [] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+
+    }
+}

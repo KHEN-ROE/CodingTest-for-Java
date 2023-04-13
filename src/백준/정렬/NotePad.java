964e1a57b83870528873d8d6d1debfb893db2cc4
KEN <shdnrnjs@gmail.com>
2023-04-14 00:02:55 +0900

    정렬 푸는중

---
+++ src/백준/정렬/NotePad.java
@@ -0,0 +1,11 @@
+package 백준.정렬;
+
+public class NotePad {
+    public static void main(String[] args) {
+        double a = 3.14;
+        double b = 3.14;
+        int c = (int) (a+b);
+        System.out.println(c);
+
+    }
+}

79b268fbe63acd14259613476ea2ae7b77a27a09
KEN <shdnrnjs@gmail.com>
2023-04-20 01:01:37 +0900

    예산 다시 하기

--- src/백준/이분탐색/예산.java
+++ src/백준/이분탐색/예산.java
@@ -8,12 +8,33 @@ import java.util.StringTokenizer;
 
 public class 예산 {
 
-
+    public static StringBuilder binarySearch(int[]arr, int m){
+        int start = 0;
+        int end = arr[arr.length - 1];
+        int answer = 0;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            int sum = 0;
+            for (int money : arr) {
+                if (money >= mid) sum += mid;
+                else sum += money;
+            }
+            if (sum > m) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+                answer = Math.max(answer, mid);
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(answer);
+        return sb;
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int [] arr = new int[n];
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
@@ -22,22 +43,7 @@ public class 예산 {
 
         Arrays.sort(arr);
 
-        int sum = 0;
-        int max = arr[0];
-        // 원소의 합계와 최댓값 구함
-        for (int i = 0; i < arr.length; i++) {
-            sum += arr[i];
-            if(arr[i] > max) max = arr[i];
-        }
-        
-        StringBuilder sb = new StringBuilder();
-        if(sum > m){
-
-        }else {
-            sb.append(max);
-        }
-        System.out.println(sb);
-
+        System.out.println(binarySearch(arr, m));
 
     }
 }
